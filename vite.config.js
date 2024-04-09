d2300c161228704ebff8d02dbd303c71da3dc8bb
Fran <fceladilla@gmail.com>
2024-04-09 09:11:41 +0200

    vite config base, url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: ".",
-  //base: "http://fceladilla.github.io/tgmediaweb",
+  //base: ".",
+  base: "http://fceladilla.github.io/tgmediaweb",
 });
